495c80f41f6ed0c322ebee01a789ec7478d4e5e1
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-01-14 16:04:08 +0000

    temporary deployent

--- src/Pages/about.tsx
+++ src/Pages/about.tsx
@@ -1,11 +1,11 @@
-import GlobeComponent from "../components/Globe"
+
 
 
 
 export const About = () => {
   return (
     <div className="container mx-auto">
-    <GlobeComponent/>
+   
   </div>
   )
 }
